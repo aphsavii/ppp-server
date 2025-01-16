4c2a9ac291809e6edc9c42876bd535ebfd31fd8f
Avinash Kumar <avinash.k95347@gmail.com>
2025-01-17 01:07:06 +0530

    change test

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,19 @@
 import { check, sleep } from 'k6';
 import http from 'k6/http';
 
-
 export const options = {
   stages: [
-    { duration: '10s', target: 150 }, // Ramp up to 50 users over 30 seconds
-    { duration: '20s', target: 150 },  // Hold at 50 users for 1 minute
-    { duration: '10s', target: 0 },  // Ramp down
+    { duration: '20s', target: 500 }, // Ramp up to 500 users over 20 seconds
+    { duration: '10s', target: 500 }, // Hold at 500 users for 10 seconds
+    { duration: '20s', target: 0 },   // Ramp down to 0 users over 20 seconds
   ],
 };
 
 export default function () {
-  const res = http.post('http://localhost:3000/aptitude/appear/28',{trade:"GCS",regno:"2331080"});
+  const res = http.post('https://pppsliet.me/aptitude/appear/28', {
+    trade: "GCS",
+    regno: "2331080",
+  });
   check(res, { 'status is 200': (r) => r.status === 200 });
   sleep(1);
 }
