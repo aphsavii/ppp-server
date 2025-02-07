4ea390941973b62a731f9cb041b0fd5905603158
Avinash Kumar <avinash.k95347@gmail.com>
2025-02-08 03:49:59 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-import cluster from 'cluster';
-import os from 'os';
 import { testConnection } from "./connections/pg-connection";
 import app from "./app";
 
-const numCPUs = os.cpus().length;
+testConnection();
+app.listen();
+app.initializeRoutes();
 
-if (cluster.isPrimary) {
-    console.log(`Master ${process.pid} is running`);
-
-    // Fork worker processes (one per CPU)
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on("exit", (worker) => {
-        console.log(`Worker ${worker.process.pid} died, restarting...`);
-        cluster.fork();
-    });
-} else {
-    console.log(`Worker ${process.pid} started`);
-    
-    // Initialize DB connection only for workers
-    testConnection();
-
-    // Start Express server
-    app.listen();
-    app.initializeRoutes();
-}
